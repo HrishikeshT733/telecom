fe97c207c8346ccbcf6a9a2b6f1e127860d5c8e9
HrishikeshT733 <hrishikeshtholbare@gmail.com>
2025-09-10 13:53:45 +0530

    Comment out unused import from App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 import './styles/globals.css';
-import Navbar from "./components/Navbar.js";
-import { useState } from 'react';
+// import Navbar from "./components/Navbar.js";
+// import { useState } from 'react';
 
 function App() {
 
